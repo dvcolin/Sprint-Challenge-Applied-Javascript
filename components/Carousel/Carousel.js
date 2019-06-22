e0df2584227775d42ab1ca7f6ba0620480c38a51
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-06-22 02:14:39 -0700

    Completed carousel stretch assignment

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,30 +1,56 @@
+class Carousel {
+    constructor(element) {
+        this.element = element;
+        const images = document.querySelectorAll('.carousel img');
+        this.images = Array.from(images).map(image => new CarouselImage(image));
+        this.currentIndex = 0;
+        this.currentImage = images[this.currentIndex];
+        rightButton.addEventListener('click', () => this.right());
+        leftButton.addEventListener('click', () => this.left());
+        console.log(this);
+    }
+
+    right() {
+        const images = document.querySelectorAll('.carousel img');
+        images.forEach(image => image.classList.remove('current-img'));
+        if (this.currentIndex === this.images.length - 1) {
+            this.currentIndex = 0;
+        } else {
+            this.currentIndex += 1;
+        }
+        this.currentImage = images[this.currentIndex];
+        this.currentImage.classList.add('current-img');
+        console.log(this);
+    }
+
+    left() {
+        const images = document.querySelectorAll('.carousel img');
+        images.forEach(image => image.classList.remove('current-img'));
+        if (this.currentIndex === 0) {
+            this.currentIndex = this.images.length - 1;
+        } else {
+            this.currentIndex -= 1;
+        }
+        this.currentImage = images[this.currentIndex];
+        this.currentImage.classList.add('current-img');
+        console.log(this);
+    }
+}
+
 class CarouselImage {
     constructor(element) {
         this.element = element;
         this.data = element.dataset.img;
         this.currentIndex = parseInt(this.data);
-        rightButton.addEventListener('click', () => this.right());
-    }
-
-    right() {
-        images.forEach(image => image.classList.remove('current-img'));
-
-        if(this.currentIndex !== (images.length - 1)) {
-            this.currentIndex += 1;
-        } else {
-            this.currentIndex = 0;
-        }
-        console.log(this.currentIndex);
-        images[this.currentIndex].classList.add('current-img');
     }
 }
 
 
-let images = document.querySelectorAll('.carousel img');
+// let images = document.querySelectorAll('.carousel img');
 const carousel = document.querySelector('.carousel');
 const leftButton = carousel.querySelector('.left-button');
 const rightButton = carousel.querySelector('.right-button');
-
+const carouselComponent = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
